17c0c72bd72affe73a7e971a19e3a877bbd74383
Võ Quang Vinh <91067804+QuangVinh555@users.noreply.github.com>
2023-05-09 18:28:44 +0700

    complete ChairStatus

---
+++ Services/ChairStatusRepository.cs
@@ -0,0 +1,38 @@
+﻿using BookMovieTickets.Data;
+using BookMovieTickets.Views;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookMovieTickets.Services
+{
+    public class ChairStatusRepository : IChairStatusRepository
+    {
+        private readonly BookMovieTicketsContext _context;
+
+        public ChairStatusRepository(BookMovieTicketsContext context)
+        {
+            _context = context;
+        }
+        public List<MessageVM> GetAllChairByHourTimeId(int HourTimeId)
+        {
+            List<MessageVM> list = new List<MessageVM>();
+            var _listChairs = _context.ChairStatuses.Where(x => x.HourTimeId == HourTimeId).ToList();
+            foreach (var item in _listChairs)
+            {
+                var _chair = new MessageVM
+                {
+                    Message = "Lấy dữ liệu thành công",
+                    Data = new ChairStatusVM
+                    {
+                        Id = item.Id,
+                        Chair = _context.Chairs.Where(x => x.Id == item.ChairId).SingleOrDefault().Name
+                    }
+                };
+                list.Add(_chair);
+            }
+            return list;
+        }
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -57,6 +57,7 @@ namespace BookMovieTickets
             services.AddScoped<IPaymentRepository, PaymentRepository>();
             services.AddScoped<IBookTicketRepository, BookTicketRepository>();
             services.AddScoped<IBookTicketDetailRepository, BookTicketDetailRepository>();
+            services.AddScoped<IChairStatusRepository, ChairStatusRepository>();
 
             services.AddSwaggerGen(c =>
             {
